c5aee632e085a99d10ebc8ce2ad22805a325053d
Vanni <48962665+VanniLynn@users.noreply.github.com>
2019-07-30 17:13:28 +0300

    Add files via upload

---
+++ Liquid image transition/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: 'img/bw1.jpg',
+    image2: 'img/bw2.jpg',
+    displacementImage: 'img/heightMap.png'
+});
